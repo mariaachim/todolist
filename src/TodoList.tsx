763a6c2a886337c84e3c6631d484ea6eb881ddad
Maria <63523678+mariaachim@users.noreply.github.com>
2022-10-28 22:11:13 +0100

    fixed bug - not updating tasks completed

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -8,38 +8,36 @@ export interface TodoListProps {
     setTasks: Setter<Task[]>;
 }
 
-let localStorageTasks: string[] = []
-
 export function TodoList(props: TodoListProps) {
     let totalTasks = props.tasks.length;
-    let completedTasks = 0;
-    props.tasks.forEach(function (task) {
-        if (task.completed) {
-            completedTasks ++;
-        };
-    });
-    console.log(completedTasks);
+
+    function SetTasksCompleted() {
+        let completedTasks = 0;
+        props.tasks.forEach(function (task) {
+            if (task.completed) {
+                completedTasks ++;
+            };
+        });
+        console.log(completedTasks);
+        return completedTasks;
+    }
+
     return (
         <><h2>Number of tasks: {totalTasks}</h2>
-        <h2>Tasks completed: {completedTasks}</h2>
+        <h2>Tasks completed: {SetTasksCompleted()}</h2>
         <For each={props.tasks}>
             {(task) => {
                 return (
                 // TODO: add button to remove task from list
-                // BUG: does not update "tasks completed"
                     <div>
                         <input type="checkbox" id={task.taskID.toString()} name="task" checked={task.completed} onChange={() => {
-                            const [taskToUpdate, setTaskToUpdate] = createSignal(props.tasks)
-                            props.setTasks((tasks) => {
-                            
+                            props.setTasks((tasks) => {  
                                 const newList = tasks.map((oldTask) =>
                                     task === oldTask ? { ...oldTask, completed: !oldTask.completed } : oldTask
                                 );
-                                console.log("changed")
                                 localStorage.setItem('storageObjects',JSON.stringify(newList));
-                                completedTasks++;
-                                console.log(newList);
-                                return newList;
+                                SetTasksCompleted();
+                                return newList
                             }); 
                         }} />
                         <label for={task.taskID.toString()}>{task.taskName}</label>
