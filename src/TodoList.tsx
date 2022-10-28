39414fd7a49e33a57e3719badd48c7f6a0087b27
Maria <63523678+mariaachim@users.noreply.github.com>
2022-10-28 13:09:40 +0100

    added tasks completed counter

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,6 +1,7 @@
 import { createSignal, For } from 'solid-js';
 import { Task, createLocalStore } from './App';
 import { AddTask } from './AddTask';
+import { Properties } from 'solid-js/web';
 
 export interface TodoListProps {
     tasks: Task[];
@@ -9,9 +10,18 @@ export interface TodoListProps {
 let localStorageTasks: string[] = []
 
 export function TodoList(props: TodoListProps) {
-    const totalTasks = () => props.tasks.length;
-    return ( // TODO: show number of tasks completed
-        <><h2>Number of tasks: {totalTasks()}</h2><For each={props.tasks}>
+    let totalTasks = props.tasks.length;
+    let completedTasks = 0;
+    props.tasks.forEach(function (task) {
+        if (task.completed) {
+            completedTasks ++;
+        };
+    });
+    console.log(completedTasks);
+    return (
+        <><h2>Number of tasks: {totalTasks}</h2>
+        <h2>Tasks competed: {completedTasks}</h2>
+        <For each={props.tasks}>
             {(task) => {
                 return (
                 // TODO: add button to remove task from list
